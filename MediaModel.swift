fd63fbe03a4d6d3cbc6d2752131bf35ce9a30423
khanhitk97 <vdkhanh.22011997@gmail.com>
2026-08-17 13:40:34 +0700

    Update MediaModel.swift

--- MediaModel.swift
+++ MediaModel.swift
@@ -5,15 +5,18 @@ enum SyncStatus: Equatable {
     case syncing(progress: Double)
     case synced(remoteURL: String)
     case failed
-} 
- 
+}
+
 struct MediaItem: Identifiable, Equatable {
     let id: String
+    var messageId: String?
     var remoteURL: String?
     var localImage: UIImage?
     var timestamp: Date
     var syncStatus: SyncStatus
     var filename: String
+    var fileSize: Int = 0
+    var isEncrypted: Bool = true
 
     static func == (lhs: MediaItem, rhs: MediaItem) -> Bool {
         lhs.id == rhs.id && lhs.syncStatus == rhs.syncStatus
